9538c16285769f88392a6ee57830125fd1332ffd
Mohan <mpalisetti@paypal.com>
2021-08-06 20:32:37 +0530

    different input & output var

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -19,7 +19,7 @@ module.exports = function configJSON(req) {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
           {
-            discount: "{{Event.APIEvent-8b05671a-3b6d-3d20-9917-4486c4b0c9b9.discountCode}}"
+            discount: "{{Event.APIEvent-8b05671a-3b6d-3d20-9917-4486c4b0c9b9.discount}}"
           }
         ],
         outArguments: [],
